9f771de7f289efc1c403d3994c9153fc885f4488
maardz <moldermarge@gmail.com>
2014-11-17 03:30:43 +0200

    commit

--- index.php
+++ index.php
@@ -7,7 +7,28 @@
  </head>
 
  <body>
+	<h2>Funktsioon "continue"</h2>
+	<?php
+ for ($number = 1; $number <= 10; $number++) {
+    if ($number == 5) {
+        continue;
+         // Kui $nr == 5, siis jääb alljärgnev vahele ning tsükkel käivitub uuesti.
+    }
 
+    echo $number . ", ";
+}
+?>
+<h2>Funktsioon "break"</h2>
+	<?php
+for ($number = 1; $number <= 10; $number++) {
+    if ($number == 6) {
+        break;
+
+    }
+
+    echo $number . ", ";
+}
+?>
 
  </body>
  </html>
